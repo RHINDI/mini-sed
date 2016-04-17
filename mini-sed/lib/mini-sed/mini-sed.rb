85cb68e9c24b623497d02aa7b6f5147a5fdd94d8
Rhindi Youssef <rhindi.youssef@courrier.uqam.ca>
2016-04-17 14:41:23 -0400

    Ajouter le module MiniSed dans lib/mini-sed/mini-sed/

---
+++ mini-sed/lib/mini-sed/mini-sed.rb
@@ -0,0 +1,23 @@
+module MiniSed
+  def self.delete( quiet, motif, lignes )
+    # A COMPLETER.
+    nil
+  end
+
+  def self.print( quiet, motif, lignes )
+    # A COMPLETER.
+    nil
+  end
+
+  def self.insert( quiet, motif, lignes )
+    # A COMPLETER.
+    nil
+  end
+
+  def self.substitute( quiet, motif, remplacement, global, lignes )
+    # A COMPLETER.
+    nil
+  end
+
+
+end
