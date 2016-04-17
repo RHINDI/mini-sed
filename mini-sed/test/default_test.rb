ca2e7d108c9df796852e870d2f81650d9535d6db
Rhindi Youssef <rhindi.youssef@courrier.uqam.ca>
2016-04-17 13:53:34 -0400

    modification Of default_test.rb

--- mini-sed/test/default_test.rb
+++ mini-sed/test/default_test.rb
@@ -1,14 +1,20 @@
 require 'test_helper'
+require 'mini-sed'
+  # class DefaultTest < Test::Unit::TestCase
+  #
+  #   def setup
+  #   end
+  #
+  #   def teardown
+  #   end
+  #
+  #   def test_the_truth
+  #     assert true
+  #   end
+  # end
 
-class DefaultTest < Test::Unit::TestCase
+describe "FOO" do
+  it "foo" do
 
-  def setup
-  end
-
-  def teardown
-  end
-
-  def test_the_truth
-    assert true
   end
 end
