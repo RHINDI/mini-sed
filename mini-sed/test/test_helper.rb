3a5db611245d9491750a95de313b41510fd295d0
Rhindi Youssef <rhindi.youssef@courrier.uqam.ca>
2016-04-17 13:47:01 -0400

    Switch from test/unit to minitest

--- mini-sed/test/test_helper.rb
+++ mini-sed/test/test_helper.rb
@@ -1,9 +1,23 @@
-require 'test/unit'
+# require 'test/unit'
+#
+# # Add test libraries you want to use here, e.g. mocha
+#
+# class Test::Unit::TestCase
+#
+#   # Add global extensions to the test case class here
+#
+# end
+gem 'minitest', '=5.4.3'
+require 'minitest/autorun'
+require 'minitest/spec'
+require 'minitest/mock'
 
-# Add test libraries you want to use here, e.g. mocha
+class Object
+  def _describe( test )
+    puts "--- On saute les tests pour \"#{test}\" ---"
+  end
 
-class Test::Unit::TestCase
-
-  # Add global extensions to the test case class here
-  
+  def _it( test )
+    puts "--- On saute le test \"#{test}\" ---"
+  end
 end
