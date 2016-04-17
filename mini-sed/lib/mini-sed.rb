f70d110a48740e9c7f5247aa8244167ae8feb042
Rhindi Youssef <rhindi.youssef@courrier.uqam.ca>
2016-04-17 14:42:11 -0400

    Add the requires

--- mini-sed/lib/mini-sed.rb
+++ mini-sed/lib/mini-sed.rb
@@ -2,3 +2,8 @@ require 'mini-sed/version.rb'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
+
+require 'debug'
+require 'dbc'
+
+require 'mini-sed/mini-sed'
